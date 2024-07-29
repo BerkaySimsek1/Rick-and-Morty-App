60931c268282082cc01340f227d8bce807f66381
BerkaySimsek1 <113512628+BerkaySimsek1@users.noreply.github.com>
2024-07-29 15:49:23 +0300

    Initial Commit

---
+++ Rick and Morty App/Rick_and_Morty_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_AppApp.swift
+//  Rick and Morty App
+//
+//  Created by Berkay on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rick and Morty AppTests/Rick_and_Morty_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_AppTests.swift
+//  Rick and Morty AppTests
+//
+//  Created by Berkay on 29.07.2024.
+//
+
+import Testing
+@testable import Rick_and_Morty_App
+
+struct Rick_and_Morty_AppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
